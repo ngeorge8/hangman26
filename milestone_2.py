0e3217641e1d5f4482a88d87c109a8f1f30b5b95
ngeorge8 <noelgeorge8@gmail.com>
2024-11-07 14:17:17 +0000

    Add milestone_2.py with list of favourite fruit

---
+++ milestone_2.py
@@ -0,0 +1,7 @@
+# milestone_2.py
+
+# Step 1: Create a list containing the names of your 5 favorite fruits
+word_list = ["apple", "banana", "mango", "strawberry", "watermelon"]
+
+# Step 2 & 3: Print out the list to the standard output
+print(word_list)
