f3ca6d715b2ed641e7bba5126581750a53e37e04
ngeorge8 <noelgeorge8@gmail.com>
2024-11-11 19:14:54 +0000

    Update milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -2,15 +2,16 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        self.word_list = word_list
-        self.num_lives = num_lives
         self.word = random.choice(word_list)
-        self.word_guessed = ['_'] * len(self.word)
+        self.word_guessed = ["_" for _ in self.word]
         self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
         self.list_of_guesses = []
 
     def check_guess(self, guess):
         guess = guess.lower()
+        
         if guess in self.word:
             print(f"Good guess! '{guess}' is in the word.")
             for index, letter in enumerate(self.word):
@@ -31,6 +32,7 @@ class Hangman:
     def ask_for_input(self):
         while True:
             guess = input("Guess a letter: ")
+           
             if len(guess) == 1 and guess.isalpha():
                 if guess in self.list_of_guesses:
                     print("You already tried that letter!")
